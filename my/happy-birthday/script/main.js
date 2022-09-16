2e8e9ca560bb8da0aa8151220e9d550c57a7287c
Rendra Yudha Adi Kalla <64743624+RendraYudha@users.noreply.github.com>
2022-09-16 18:57:02 +0700

    Update main.js

--- my/happy-birthday/script/main.js
+++ my/happy-birthday/script/main.js
@@ -283,14 +283,7 @@ const animationTimeline = () => {
       zIndex: "-1"
     })
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
-    .to(
-      ".last-smile",
-      0.5,
-      {
-        rotation: 90
-      },
-      "+=1"
-    );
+    
 
   // tl.seek("currentStep");
   // tl.timeScale(2);
